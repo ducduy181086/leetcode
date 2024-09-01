cdbf7e182dee5539acc87fefcff2558f6820905c
Duc Duy Nguyen <ducduy181086@gmail.com>
2024-09-01 09:43:38 -0500

    exercise: Container With Most Water

---
+++ container-with-most-water/container-with-most-water/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using container_with_most_water;
+
+using PracticeLibrary.Helpers;
+
+var writer = Console.Out;
+var cases = await GetCases(CancellationToken.None);
+var solution = new Solution();
+foreach (var item in cases) {
+    writer.WriteLine("height =");
+    item.Print(writer);
+    writer.WriteLine("Output");
+    writer.WriteLine(solution.MaxArea(item));
+}
+
+async Task<ICollection<int[]>> GetCases(CancellationToken cancellation) {
+    var result = new List<int[]>();
+    var readers = TestHelper.GetTestReaders();
+    foreach (var reader in readers) {
+        result.Add(await reader.GetIntArrayAsync(cancellation));
+    }
+
+    return result;
+}
---
+++ container-with-most-water/container-with-most-water/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace container_with_most_water {
+    public class Solution {
+        public int MaxArea(int[] height) {
+            int maxArea = 0;
+            int left = 0;
+            int right = height.Length - 1;
+            while (left < right) {
+                int width = right - left;
+                int minHeight = Math.Min(height[left], height[right]);
+                maxArea = Math.Max(maxArea, width * minHeight);
+                if (height[left] <= height[right]) { left++; }
+                else { right--; }
+            }
+            return maxArea;
+        }
+    }
+}
