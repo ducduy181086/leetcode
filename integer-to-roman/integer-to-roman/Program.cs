7d5bbda9504446908e57bf2b035fc2e09b5f0627
Duc Duy Nguyen <ducduy181086@gmail.com>
2024-08-31 22:51:35 -0500

    Integer to Roman

---
+++ integer-to-roman/integer-to-roman/Program.cs
@@ -0,0 +1,28 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using integer_to_roman;
+
+using PracticeLibrary.Helpers;
+
+var writer = Console.Out;
+var cases = await GetCases(CancellationToken.None);
+var solution = new Solution();
+foreach (var item in cases)
+{
+    writer.WriteLine("num =");
+    writer.WriteLine(item);
+    writer.WriteLine("Output");
+    writer.WriteLine(solution.IntToRoman(item));
+}
+
+async Task<ICollection<int>> GetCases(CancellationToken cancellation)
+{
+    var result = new List<int>();
+    var readers = TestHelper.GetTestReaders();
+    foreach (var reader in readers)
+    {
+        result.Add(await reader.GetIntAsync(cancellation));
+    }
+
+    return result;
+}
---
+++ integer-to-roman/integer-to-roman/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+
+namespace integer_to_roman
+{
+    public class Solution
+    {
+        public string IntToRoman(int num)
+        {
+            var sb = new StringBuilder();
+            int i = 0;
+            while (num > 0)
+            {
+                int digits = num % 10;
+                if (i == 0)
+                {
+                    Update(digits, 'I', 'V', 'X', sb);
+                }
+                else if (i == 1)
+                {
+                    Update(digits, 'X', 'L', 'C', sb);
+                }
+                else if (i == 2)
+                {
+                    Update(digits, 'C', 'D', 'M', sb);
+                }
+                else
+                {
+                    Update(digits, 'M', 'M', 'M', sb);
+                }
+
+                i++;
+                num /= 10;
+            }
+            return sb.ToString();
+        }
+
+        private void Update(int num, char one, char five, char ten, StringBuilder sb)
+        {
+            int index = 0;
+            if (num <= 3)
+            {
+                for (int i = 0; i < num; i++) sb.Insert(index++, one);
+            }
+            else if (num == 4)
+            {
+                sb.Insert(index++, one); sb.Insert(index++, five);
+            }
+            else if (5 <= num && num < 9)
+            {
+                sb.Insert(index++, five);
+                for (int i = 0; i < num - 5; i++) sb.Insert(index++, one);
+            }
+            else
+            {
+                sb.Insert(index++, one); sb.Insert(index++, ten);
+            }
+        }
+    }
+}
