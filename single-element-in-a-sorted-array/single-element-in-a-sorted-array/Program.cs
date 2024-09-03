0e23d02d92e90daa981bf98fb465be1a384f3bc0
Duc Duy Nguyen <ducduy181086@gmail.com>
2024-09-02 22:21:28 -0500

    Single Element in a Sorted Array

---
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PracticeLibrary.Helpers;
+
+using single_element_in_a_sorted_array;
+
+var writer = Console.Out;
+var cases = await GetCases(CancellationToken.None);
+var solution = new Solution();
+foreach (var item in cases) {
+    writer.WriteLine("nums =");
+    item.Print(writer);
+    writer.WriteLine("Output");
+    writer.WriteLine(solution.SingleNonDuplicate(item));
+}
+
+async Task<ICollection<int[]>> GetCases(CancellationToken cancellation) {
+    var result = new List<int[]>();
+    var readers = TestHelper.GetTestReaders();
+    foreach (var reader in readers) {
+        result.Add(await reader.GetIntArrayAsync(cancellation));
+    }
+
+    return result;
+}
---
+++ single-element-in-a-sorted-array/single-element-in-a-sorted-array/Solution.cs
@@ -0,0 +1,31 @@
+﻿namespace single_element_in_a_sorted_array {
+    public class Solution {
+        public int SingleNonDuplicate(int[] nums) {
+            if (nums.Length == 1) return nums[0];
+            int left = 0;
+            int right = nums.Length - 1;
+            int mid;
+            while (left < right) {
+                mid = left + (right - left) / 2;
+                if (mid % 2 == 0) {
+                    if (nums[mid] == nums[mid + 1]) {
+                        left = mid + 2;
+                    }
+                    else {
+                        right = mid;
+                    }
+                }
+                else {
+                    if (nums[mid] == nums[mid - 1]) {
+                        left = mid + 1;
+                    }
+                    else {
+                        right = mid;
+                    }
+                }
+            }
+
+            return nums[left];
+        }
+    }
+}
